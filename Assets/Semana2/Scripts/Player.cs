abb224f60307b7a178bcdacb52d35c93f88eb1a1
WICHOLAP\Raul C <lraul.castillo@gmail.com>
2023-04-17 11:15:49 -0600

    movimiento

--- Assets/Semana2/Scripts/Player.cs
+++ Assets/Semana2/Scripts/Player.cs
@@ -8,6 +8,13 @@ public class Player : MonoBehaviour
     [Range(0.0f,10.0f)]
     [SerializeField] private float velocidad;
 
+    // Limites de la pantalla para mi jugador
+    float limiteHorizontal = -8.329f;
+    float limiteVertical = 4.53f;
+
+    int a = 3;
+    int b = 5;
+
     // Update is called once per frame
     void Update()
     {
@@ -17,8 +24,34 @@ public class Player : MonoBehaviour
         Vector3 direccion = new Vector3(inputHorizontal, inputVertical);
 
         //Frame independant
-        transform.Translate(direccion * (velocidad * Time.deltaTime));
 
-        print($"Valor Horizontal: {inputHorizontal}\nValor Vertical: {inputVertical}");
+        if (transform.position.x > limiteHorizontal && transform.position.x < limiteHorizontal * -1)
+        {
+            transform.Translate(direccion * (velocidad * Time.deltaTime));
+        }
+        else
+        {
+            print("Ya se atoró");
+        }
+
+
+
+        //if (Input.GetKeyDown(KeyCode.Space))
+        //{
+            //a = a + 1;
+         //   a += 1;
+         //   print(a);
+
+         //   if (a > b)
+         //   {
+         //       print("Verdadero");
+         //   }
+         //   else
+         //   {
+         //       print("Falso");
+          //  }
+
+       // }
+
     }
 }
