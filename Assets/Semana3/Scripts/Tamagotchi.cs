a52cea97d07688ef68e75cedc66965ed6964590e
WICHOLAP\Raul C <lraul.castillo@gmail.com>
2023-04-21 11:06:40 -0600

    tamagochi casi terminado

---
+++ Assets/Semana3/Scripts/Tamagotchi.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tamagotchi : MonoBehaviour
+{
+    [SerializeField] Animator tamagotchiAnimator;
+    [SerializeField] GameObject comida;
+    public void Dormir()
+    {
+        print("Dormir");
+        tamagotchiAnimator.SetTrigger("triggerDormir");
+
+        Invoke("RegresoIdle", 2.0f);
+    }
+    public void Shower()
+    {
+        print("Shower");
+    }
+    public void Comer()
+    {
+        print("Comer");
+
+        tamagotchiAnimator.SetTrigger("triggerComer");
+        comida.SetActive(true);
+
+
+        //Invoke("RegresoIdle", 2.0f);
+        //Invoke("ApagarComida", 2.0f);
+
+
+        Invoke("ApagarComida", 2.0f);
+
+    }
+
+    private void RegresoIdle()
+    {
+        tamagotchiAnimator.SetTrigger("regresoIdle");
+    }
+
+    private void ApagarComida()
+    {
+        RegresoIdle();
+        comida.SetActive(false);
+    }
+}
